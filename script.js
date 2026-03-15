97ddec03f104b08e0329d043a463282602912f2c
Arish Amin <arishmeghani@gmail.com>
2026-03-15 10:49:50 +0500

    Guess the Game Logic and Main Functionality

---
+++ script.js
@@ -0,0 +1,48 @@
+'use strict';
+let correctNumber = Math.floor(Math.random()*20)+1
+let score = 20
+
+
+console.log(correctNumber)
+
+document.querySelector('.check').addEventListener('click', function(){
+    let guess = Number(document.querySelector('.guess').value)
+    
+    if(guess===correctNumber){
+     document.body.style.backgroundColor= "green"
+     document.querySelector('.message').textContent = "Correct Answer... Play Again?"
+     document.querySelector('.number').textContent = correctNumber
+     document.querySelector('.highscore').textContent = score
+     
+    }
+    else if(guess>correctNumber){
+    if(score>1){
+     score--
+     document.querySelector('.message').textContent = "Lower"
+     document.querySelector('.score').textContent = score
+     
+     
+
+    }
+    else{
+        document.body.style.backgroundColor= "darkred"
+        document.querySelector('.message').textContent = "Game Lost Click on Try Again to Restart..."
+        document.querySelector('.score').textContent = 0
+       
+
+    }
+    }
+    else if(guess<correctNumber){
+    if(score>1){
+     score --
+     document.querySelector('.message').textContent = "Higher" 
+     document.querySelector('.score').textContent = score
+    
+    }
+    else{
+        document.body.style.backgroundColor= "darkred"
+        document.querySelector('.message').textContent = "Game Lost Click on Try Again to Restart"
+        document.querySelector('.score').textContent = 0
+    }
+    }
+})
